afcab6fc87ec999c0f284ae71fb794a2337e22a2
cookedsteak <cookedsteak708@gmail.com>
2018-10-23 11:18:05 +0800

    update bash

--- singular/src/solc-compile.sh
+++ singular/src/solc-compile.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-solc --bin -o tmp --overwrite --optimize --optimize-runs 1 contracts=/Users/bran/projects/udap/EIP/singular/src/contracts  openzeppelin-solidity=/Users/bran/projects/udap/EIP/singular/src/node_modules/openzeppelin-solidity ./contracts/**/*.sol
+CONTRACTS=$PWD/contracts
+OPENZEPPELIN=$PWD/node_modules/openzeppelin-solidity
+solc --bin -o tmp --overwrite --optimize --optimize-runs 1 contracts=$CONTRACTS openzeppelin-solidity=$OPENZEPPELIN ./contracts/**/*.sol
