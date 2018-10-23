311bfa5d98fba23446c94a9dbadc6b060d39cf01
cookedsteak <cookedsteak708@gmail.com>
2018-10-23 11:31:40 +0800

    optimize bash

--- singular/src/solc-compile.sh
+++ singular/src/solc-compile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-CONTRACTS=$PWD/contracts
-OPENZEPPELIN=$PWD/node_modules/openzeppelin-solidity
-solc --bin -o tmp --overwrite --optimize --optimize-runs 1 contracts=$CONTRACTS openzeppelin-solidity=$OPENZEPPELIN ./contracts/**/*.sol
+
+if [ ! -d "$PWD/node_modules" ]; then
+  printf "==ERROR==\\nOps, \`node_modules\` not found. Please run \`npm install\` first.\\n==ERROR==\\n"
+  exit 1
+fi
+
+CONTRACTS=${PWD}/contracts
+OPENZEPPELIN=${PWD}/node_modules/openzeppelin-solidity
+
+solc --bin -o tmp --overwrite --optimize --optimize-runs 1 contracts=${CONTRACTS} openzeppelin-solidity=${OPENZEPPELIN} ./contracts/**/*.sol
